56661d90aea28083bd0f089734463e0ebd1d43e1
pnewsam <paul.newsam@gmail.com>
2023-06-18 11:33:11 -0700

    Add safearea, imgs, and font

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts, Jost_400Regular } from "@expo-google-fonts/jost";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Jost_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text style={{ fontFamily: "Jost_400Regular", fontSize: 40 }}>
+          Jost text here
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
